935f1bdaf62274d3856b6a8688fcd408e6289c23
Brian Job Okello <63491479+BrianPollar@users.noreply.github.com>
2024-05-11 15:10:52 +0300

    payment refactor

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,11 +1,33 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
-import { getLogger } from 'log4js';
+import * as fs from 'fs';
+import * as tracer from 'tracer';
 import { IendpointResponse, IgetIpnEndPointsRes, IgetTokenRes, IgetTransactionStatusRes, IipnResponse, IorderResponse, IpayDetails, IpesaPalError, IpesaPalToken, IrefundRequestReq, IrefundRequestRes, IrefundRequestResComplete, IregisterIpnRes, IrelegateTokenStatusRes, IsubmitOrderRes, TnotificationMethodType } from '../interfaces/general.interface';
 import { Iconfig, Pesapal } from '../pesapal';
 
-const logger = getLogger('paymentController');
+const logger = tracer.colorConsole(
+  {
+    format: '{{timestamp}} [{{title}}] {{message}} (in {{file}}:{{line}})',
+    dateformat: 'HH:MM:ss.L',
+    transport(data) {
+      // eslint-disable-next-line no-console
+      console.log(data.output);
+      const logDir = './serverLog/';
+      fs.mkdir(logDir, { recursive: true }, (err) => {
+        if (err) {
+          if (err) {
+            throw err;
+          }
+        }
+      });
+      fs.appendFile('./serverLog/pesapal.log', data.rawoutput + '\n', err => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+  });
 
 /**
  * This function creates a mock pay details object.
